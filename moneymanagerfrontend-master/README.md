779709afa199d944405b6147ee41db260f272170
Aditya Verma <adityaofficial835@gmail.com>
2026-01-03 17:57:14 +0530

    Add files via upload

---
+++ moneymanagerfrontend-master/README.md
@@ -0,0 +1,23 @@
+# Money Manager Frontend
+
+A simple frontend application built using React to manage income and expenses.
+
+## Tech Stack
+- React
+- JavaScript
+- HTML
+- CSS
+
+## Features
+- Add income and expenses
+- View transaction history
+- Simple and clean UI
+
+
+## How to Run the Project
+1. Clone the repository
+2. Open the project folder in VS Code
+3. Run `npm install`
+4. Run `npm run dev`
+5. Open `http://localhost:5173` in the browser
+
---
+++ moneymanagerfrontend-master/src/components/Dashboard.jsx
@@ -0,0 +1,24 @@
+import Menubar from "./Menubar.jsx";
+import {useContext} from "react";
+import {AppContext} from "../context/AppContext.jsx";
+import Sidebar from "./Sidebar.jsx";
+const  Dashboard =({children,activeMenu})=>{
+    const {user} = useContext(AppContext); 
+    return (
+    <div>
+        <Menubar activeMenu={activeMenu} />
+         {user && (
+                         <div className="flex">
+                             <div className="max-[1080px]:hidden">
+                                 <Sidebar activeMenu={activeMenu}/>
+                             </div>
+         
+                             <div className="grow mx-5">{children}</div>
+                         </div>
+                     )}
+                        
+                   
+    </div>
+    )
+}
+export default Dashboard;
